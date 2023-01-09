8422925d0ed905f310eedaa32ccb3bc1a01c8625
xue runyuan <603022844@qq.com>
2023-01-09 21:13:08 +0800

    ​git config pull.rebase false​

---
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using BetterFurniture.Areas.Identity.Data;
+using BetterFurniture.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(BetterFurniture.Areas.Identity.IdentityHostingStartup))]
+namespace BetterFurniture.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<BetterFurnitureContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("BetterFurnitureContextConnection")));
+
+                services.AddDefaultIdentity<BetterFurnitureUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<BetterFurnitureContext>();
+            });
+        }
+    }
+}
